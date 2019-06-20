6485501765f4832494c18aafe24380ba9a216d4e
collins njau <collinsnjau39@gmail.com>
2019-06-20 18:15:53 +0300

    Inject model into the service

--- app/Services/EloquentTodo.php
+++ app/Services/EloquentTodo.php
@@ -4,23 +4,38 @@ use App\Repository\TodoInterface;
 
 class EloquentTodo implements TodoInterface
 {
+
+  private $model;
+  // inject model inside Implementation of the 
+  // Interface
+  public function __construct(Todo $todo)
+  {
+    $this->model = $todo;
+  }
   public function getAll(){
+    return $this->model->all();
 
   }
 
   public function getById($id){
+    return $this->model->findById($id);
 
   }
 
   public function create(array $attributes){
+    return $this->model->create($attributes);
 
   }
 
   public function update($id, array $attributes){
-
+    $todo = $this->model->findOrFail($id);
+    $todo->update($attributes);
+    return $todo;
   }
 
   public function delete($id){
-    
+
+    $this->model->getById($id)->delete();
+    return true;
   }
 }
