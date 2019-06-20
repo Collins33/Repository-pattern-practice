06c33fae719851e681f8fc5852025073ddd5115f
collins njau <collinsnjau39@gmail.com>
2019-06-20 18:01:25 +0300

    Add Todo service

---
+++ app/Services/EloquentTodo.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Services;
+use App\Repository\TodoInterface;
+
+class EloquentTodo implements TodoInterface
+{
+  public function getAll(){
+
+  }
+
+  public function getById($id){
+
+  }
+
+  public function create(array $attributes){
+
+  }
+
+  public function update($id, array $attributes){
+
+  }
+
+  public function delete($id){
+    
+  }
+}
